1ac4135f607220c64eab9784e839ab090f418e3e
Kristina Papić <papic.kris@gmail.com>
2018-05-18 15:12:27 +0200

    updated model for core

---
+++ core/src/main/java/XMLWS/model/Reservation.java
@@ -0,0 +1,55 @@
+package XMLWS.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Reservation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@OneToOne
+	private Period period;
+	
+	@ManyToOne
+	private User user;
+
+	public Reservation(Period period, User user) {
+		super();
+		this.period = period;
+		this.user = user;
+	}
+	
+	public Reservation(){}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Period getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(Period period) {
+		this.period = period;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
